2fd0b0b5a3e19cdffbb8e13e6f4c438f7237bb7a
Kayro Figueroa <kayrofigueroa@gmail.com>
2019-10-05 11:08:29 -0700

    Test

---
+++ Spotlight test 1/java.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+
+var spotlight = document.getElementById("spotlight");
+var spotlight_child = document.getElementById("spotlight-child");
+var main = document.getElementById("main");
+main.addEventListener("mousemove", moveSpotlight);
+main.addEventListener("touchmove", moveSpotlight);
+function moveSpotlight(e) {
+    e.preventDefault();
+    let pos, x, y;
+  x = e.clientX - 300;
+  y = e.clientY - 350;
+  spotlight.style.left = x + "px";
+  spotlight.style.top = y + "px";
+  spotlight_child.style.left = x + "px";
+  spotlight_child.style.top = y + "px";
+}
+
+});
